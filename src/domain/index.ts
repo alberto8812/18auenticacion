31f0f391fdc00a586131be2552e4f570216c5c27
alberto8812 <101751073+alberto8812@users.noreply.github.com>
2024-04-16 23:55:02 -0500

    create method login

--- src/domain/index.ts
+++ src/domain/index.ts
@@ -3,4 +3,5 @@
 
 export * from './errors/custom.error';
 export * from './dtos/auth/register-user.dto';
+export * from './dtos/auth/login-user.dto';
 export * from './entities/uese.entity';
--- src/presentation/service/auth.service.ts
+++ src/presentation/service/auth.service.ts
@@ -2,28 +2,54 @@
 
 import { bcryptAdapter } from "../../config";
 import { ueserModel } from "../../data";
-import { CustomError, RegisterUserDto, UserEntity } from "../../domain";
+import {
+  CustomError,
+  RegisterUserDto,
+  UserEntity,
+  LoginUserDto,
+} from "../../domain";
 
 export class AuthService {
   constructor() {}
 
+  private async userExist(email: string) {
+    const existUser = await ueserModel.findOne({ email });
+    if (!existUser) throw CustomError.badRequest("Email not exist");
+    return UserEntity.fromObject(existUser!);
+  }
   public async registerUser(registerUser: RegisterUserDto) {
+    await this.userExist(registerUser.email);
     const existUser = await ueserModel.findOne({ email: registerUser.email });
     if (existUser) throw CustomError.badRequest("Email already exist");
     try {
-        const user= new ueserModel(registerUser);//
-        //encriptar la contrasena
-        user.password=bcryptAdapter.hash(registerUser.password);
-        await user.save();
-        // jwt<-- para mantener la autencacion del usuario
-        
-        // Email confirmacion
+      const user = new ueserModel(registerUser); //
+      //encriptar la contrasena
+      user.password = bcryptAdapter.hash(registerUser.password);
+      await user.save();
+      // jwt<-- para mantener la autencacion del usuario
 
-        const {password,...userEntity}=UserEntity.fromObject(user);
-        return {...userEntity}
+      // Email confirmacion
+
+      const { password, ...userEntity } = UserEntity.fromObject(user);
+      return { ...userEntity };
     } catch (error) {
-        console.log(error)
-        throw CustomError.internalServer(`${error}`)
+      console.log(error);
+      throw CustomError.internalServer(`${error}`);
     }
+  }
+  public async LoginUser(loginUserDto: LoginUserDto) {
+    //si existe
+    const existUser = await this.userExist(loginUserDto.email);
+    //ismatch bcrp compare
+    const isEqualPassword = bcryptAdapter.compare(
+      loginUserDto.password,
+      existUser.password
+    );
+    if (!isEqualPassword) throw CustomError.unautorized(`password not mach`);
+    //retornar
+    const { password, ...rest } = existUser;
+    return {
+      user: { ...rest },
+    };
   }
 }
